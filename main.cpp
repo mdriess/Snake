58d842f1c30db9551efeba5656cdf6cacca098c0
Luke Willson <Luke_Willson@baylor.edu>
2012-10-12 15:30:20 -0500

    Added "Main" and "Plotter"
    This is basically a CodeBlocks project. If you guys don't use
    CodeBlocks, all of the code is still here for you to edit.

---
+++ main.cpp
@@ -0,0 +1,44 @@
+/*
+Authors:Ben Cody
+        Matthew Roberts
+        Mike Riess
+        Luke Willson
+Assignment Title: Snake
+Assignment Description:Snake is a video game that originated during the late
+1970s in arcades and has maintained popularity since then, becoming something
+of a classic. After it became the standard pre-loaded game on Nokia phones in
+1998, Snake found a massive audience. The player controls a long, thin
+creature, resembling a snake, which roams around on a bordered plane, picking
+up food (or some other item), trying to avoid hitting its own tail or the
+"walls" that surround the playing area. Each time the snake eats a piece
+of food, its tail grows longer, making the game increasingly difficult. The
+user controls the direction of the snake's head (up, down, left, or right),
+and the snake's body follows. The player cannot stop the snake from moving
+while the game is in progress, and cannot make the snake go in reverse.
+However, Snake has had many variations since its release, depending on the
+game's platform. These variations involve the modification of certain rules
+e.g. the lethality of contact with walls.
+Due Date: 11/26/12
+Date Created: 10/12/12
+Date Last Modified: 10/12/12
+
+Data Abstraction:
+Input:
+Process:
+Output:
+Assumptions:
+*/
+
+#include <iostream>
+#include "plotter.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    //INSERT SNAKE HERE
+
+    return 0;
+}
