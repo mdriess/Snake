ac34c2d3a58101f6476bca2ef2349a2a3758b8de
Matthew Roberts <Matthew_Roberts@baylor.edu>
2012-10-25 09:27:00 -0500

    Added game
    Added a stub to the Game class.  It has a method playGame which receives
    a User object and then procedes to play the game.

---
+++ game.cpp
@@ -0,0 +1,41 @@
+/*
+Authors:Ben Cody
+        Matthew Roberts
+        Mike Riess
+        Luke Willson
+Assignment Title: Snake
+Assignment Description:Snake is a video game that originated during the late
+1970s in arcades and has maintained popularity since then, becoming something
+of a classic. After it became the standard pre-loaded game on Nokia phones in
+1998, Snake found a massive audience. The player controls a long, thin
+creature, resembling a snake, which roams around on a bordered plane, picking
+up food (or some other item), trying to avoid hitting its own tail or the
+"walls" that surround the playing area. Each time the snake eats a piece
+of food, its tail grows longer, making the game increasingly difficult. The
+user controls the direction of the snake's head (up, down, left, or right),
+and the snake's body follows. The player cannot stop the snake from moving
+while the game is in progress, and cannot make the snake go in reverse.
+However, Snake has had many variations since its release, depending on the
+game's platform. These variations involve the modification of certain rules
+e.g. the lethality of contact with walls.
+Due Date: 11/26/12
+Date Created: 10/24/12
+Date Last Modified: 10/25/12
+Last modified by: Matthew Roberts
+
+Data Abstraction:
+Input:
+Process:
+Output:
+Assumptions:
+*/
+
+#include <iostream>
+#include "plotter.h"
+#include "game.h"
+#include "user.h"
+
+void Game::playGame(User& user)
+{
+     screen.plot(10,10,'!');
+}
